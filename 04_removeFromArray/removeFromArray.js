0a97ef0275c3ca1d01862e3767b5eab9f22e73d2
supnoodles <supnoodless@gmail.com>
2022-08-02 20:39:18 +0100

    feat: ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
 
+    let index;
+
+    for (let i = 0; i < args.length; ++i) {
+        index = arr.indexOf(args[i]);
+        if (index != -1) {
+            arr.splice(index, 1)
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
