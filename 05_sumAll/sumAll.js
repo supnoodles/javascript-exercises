78390021724ad7c2b99950404532310f62cd0a64
supnoodles <supnoodless@gmail.com>
2022-08-02 20:39:28 +0100

    feat: ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number"
+        || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+
+    for (let i = num1; i <= num2; ++i) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
