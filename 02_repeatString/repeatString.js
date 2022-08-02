a8f567bf8f9862b62657fa74cf6bece02ad0df3d
supnoodles <supnoodless@gmail.com>
2022-08-02 20:38:56 +0100

    feat: ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (_str, times) {
+    if (times == 0) {
+        return ''
+    } else if (times == -1) { 
+        return 'ERROR'
+    } else {
+        return _str.repeat(times);
+    }
 };
 
 // Do not edit below this line
