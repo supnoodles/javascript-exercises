848617db368ee28d7f3d7796a49392c317c46cdb
supnoodles <supnoodless@gmail.com>
2022-08-03 10:36:40 +0100

    feat: ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+    
+    if (x <= 0) {
+        return 'OOPS'
+    }
 
+    if (typeof x == "string") {
+        x = Number(x);
+    }
+
+    if (x == 1 || x == 2) {
+        return 1;
+    }
+
+    let a = 1;
+    let b = 1;
+    let c = 2;
+
+    for (let i = 0; i < x - 2; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return c;
 };
 
 // Do not edit below this line
