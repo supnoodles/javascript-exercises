61326525028db4b9e35d9c1865fd091e9cc713b3
supnoodles <supnoodless@gmail.com>
2022-08-03 10:36:19 +0100

    feat: ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let mult = 1;
+  
+  for (let i = 0; i < arr.length; ++i) {
+    mult *= arr[i];
+  }
+
+  return mult;
 
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  
+  if (x == 0) {
+    return 1;
+  }
+
+  let factorial = 1;
+
+  while (x > 0) {
+    factorial *= x;
+    --x;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
