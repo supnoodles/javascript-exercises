73434313836d792580aa6e5fa649a3c8917b7a23
supnoodles <supnoodless@gmail.com>
2022-08-03 10:36:10 +0100

    feat: ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(celsius) {
+  return Math.round(((celsius - 32) * 0.5556) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(fahrenheit) {
+  return Math.round(((fahrenheit * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
