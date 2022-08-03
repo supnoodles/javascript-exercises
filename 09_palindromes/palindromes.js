c5706125707b4c47bad57a855b1efa9150a9e49f
supnoodles <supnoodless@gmail.com>
2022-08-03 10:36:31 +0100

    feat: ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let clean_word = word.toLowerCase().trim().replace(/[^a-zA-Z]+/g, '');
+    let reversed_word = clean_word.split('').reverse().join('');
+    return clean_word == reversed_word;
+    
 
 };
 
